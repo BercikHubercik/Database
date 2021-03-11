78f3792382c07716ae3805dc6d2f1c15aab31d0d
BercikHubercik <hubertstep@gmail.com>
2021-03-11 01:48:53 +0100

    moved db.js to correct dir

---
+++ db.js
@@ -0,0 +1,27 @@
+const database = {
+
+
+    add(pk, type, id, obj) {
+        if (database[pk] === undefined) {
+            database[pk] = {};
+            database[pk][type] = {};
+            database[pk][type][id] = obj;
+        } else {
+            if (database[pk][type] === undefined) {
+                database[pk][type] = {};
+                database[pk][type][id] = obj;
+            } else {
+                database[pk][type][id] = obj;
+            }
+        }
+    },
+    get(pk, type, id) {
+        return database[pk][type][id];
+    },
+    list(pk, type) {
+        return database[pk][type];
+    }
+    //todo:
+    //filter(pk, type, simpleExpression){}
+    //query(){}
+};
